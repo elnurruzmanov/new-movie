2d6783743cd5ec5c6b4f8c3809a74a1f51e4e1b9
elnurruzmanov@yandex.ru <elnurruzmanov219autp@gmail.com>
2022-04-18 12:49:51 +0300

    sasa

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,9 +7,22 @@ class Movie extends Component {
     const { movies } = this.props;
     return (
       <div className="movies">
-        {movies.map((movie) => (
-          <Movies key={movie.imdbID} {...movie} />
-        ))}
+        {movies.length ? (
+          movies.map((movie) => <Movies key={movie.imdbID} {...movie} />)
+        ) : (
+          <div className="the-width">
+            <div className="">
+              <div className="four04">
+                <h1>404</h1>
+                <div className="target-cont">
+                  <div className="cover"></div>
+                  <div className="marks"></div>
+                </div>
+              </div>
+              <h2>The page you requested was not found.</h2>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
